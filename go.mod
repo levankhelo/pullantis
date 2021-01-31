727e61e6eadd6551d3f102fd37719cc78612d9ba
levan khelashvili <levan.khelashvili@cern.ch>
2021-01-31 19:17:09 +0400

    updated

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module pullantis
 go 1.14
 
 require (
-	github.com/google/go-github/v33 v33.0.0 // indirect
+	github.com/go-git/go-git v4.7.0+incompatible
+	github.com/go-git/go-git/v5 v5.2.0 // indirect
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-github/v33 v33.0.0
 	github.com/m7shapan/njson v1.0.3 // indirect
 	github.com/pulumi/pulumi-github/sdk v1.5.0
 	github.com/pulumi/pulumi-terraform/sdk/v2 v2.5.0 // indirect
 	github.com/pulumi/pulumi/sdk v1.14.1
 	github.com/pulumi/pulumi/sdk/v2 v2.19.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 	golang.org/x/tools v0.1.0 // indirect
 	gopkg.in/rjz/githubhook.v0 v0.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,33 +2,168 @@ package main
 
 // import libraries
 import (
-
-	// njson "github.com/m7shapan/njson"
 	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
 	f "fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
-	"github.com/google/go-github/v33/github"
+	github "github.com/google/go-github/v33/github"
 	"golang.org/x/oauth2"
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
-// webhook handler
-// referrenced to https://groob.io/tutorial/go-github-webhook/
+//// STRUCTURES
+
+// Queue primitive data structure
+type Queue struct {
+	list   []interface{}
+	length int
+}
+
+func (q Queue) push(el interface{}) (qu Queue) {
+	q.list = append(q.list, el)
+	q.length++
+	q.disp()
+	return q
+}
+func (q Queue) pop() (qu Queue) {
+
+	if q.length > 1 {
+		q.list = q.list[1:]
+	} else {
+		q.list = make([]interface{}, 0)
+	}
+	q.length--
+	q.disp()
+	return q
+}
+func (q Queue) disp() {
+	f.Printf("Queue: %v\n", q)
+}
+
+//// FUNCTIONS
+
+// SetupCloseHandler - handle clean up after exit
+func SetupCloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	// new thread for cleanup
+	go func() {
+		<-c
+		f.Println("\r- Ctrl+C pressed in Terminal")
+		// remove temp directory
+		os.RemoveAll("./tmp/")
+		f.Println("Exited")
+		os.Exit(0)
+	}()
+}
+
+// goCloneGitRepo - Clone GitHub Repo in tmp/ directory
+func goCloneGitRepo(tokenGit string, repo *github.Repository) {
+	f.Printf("\n\n%v\n\n", *repo.CloneURL)
+	cloneurl := *repo.CloneURL
+	gitCommand := cloneurl[:8] + tokenGit + "@" + cloneurl[8:]
+	cmd := exec.Command("git", "clone", gitCommand)
+	cmd.Run()
+	f.Println("Git: Clonned ", cloneurl)
+}
+
+// goGitLogin - log in to git and return client
+func goGitLogin(token string) (user *github.Client, contx context.Context) {
+	/*
+		Args:
+			token: git token
+		Returns:
+			user: github client
+			contx: context
+	*/
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+
+	// authenticate new client using context and token
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc), ctx
+}
+
+// goGitCheckout - checkout clonned repo to target branch
+func goGitCheckout(branch string, project string) {
+
+	// move to git directory
+	os.Chdir("tmp/" + project)
+	f.Println("Moved in ", "tmp/"+project, "directory")
+
+	// execute command to checkout on branch
+	cmd := exec.Command("git", "checkout", branch)
+	cmd.Run()
+	f.Println("Git: check out on branch", branch)
+
+	// move back to work directory
+	os.Chdir("../../")
+	f.Println("Moved back to work directory")
+}
+
+// ?? goGetGitRepo - get github repo we need as variable
+func goGetGitRepo(ctx context.Context, gitClient *github.Client, targetRepo string) (rep github.Repository) {
+	// get repository we are looking for
+	repos, _, err := gitClient.Repositories.List(ctx, "", nil)
+	if err != nil {
+		f.Println("Could not authenticate git")
+		return
+	}
+	var repo github.Repository
+	for i := 0; i < len(repos); i++ {
+		f.Println(*repos[i].Name)
+		if *repos[i].Name == targetRepo {
+			repo = *repos[i]
+		}
+	}
+	return repo
+}
+
+func runPulumiPlan(project string) {
+	os.Chdir("tmp/" + project)
+}
+
+var queue Queue
+
+// pull request structure
+type GitPL struct {
+	project string
+	branch  string // branch name -
+	action  string // action done: comment pull request close
+	ID      int    // ID of pull request -> pull_request.number
+}
+
+func commentOnReview() {
+
+}
+
+func handleQueue(branch string, project string) {
+	if queue.length == 0 {
+		goGitCheckout(branch, project)
+		runPulumiPlan(project)
+		return
+	}
+	f.Println("NOPE")
+	queue = queue.push(branch)
+}
+
+// referrence - https://groob.io/tutorial/go-github-webhook/
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	// f.Printf("\n\nreceived %v\n\n", m)
 	f.Println("HookHandler: Data received")
 	// f.Printf("headers: %v\n", r.Header)
-	// f.Printf("git event: %v\n", r.Header.Get("X-Github-Event"))
+	f.Printf("git event: %v\n", r.Header.Get("X-Github-Event"))
 	webhookData := make(map[string]interface{})
-	// var webhookData webhook
 
 	err := json.NewDecoder(r.Body).Decode(&webhookData)
 	if err != nil {
@@ -36,25 +171,17 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var gitProjectName string = webhookData["pull_request"].(map[string]interface{})["head"].(map[string]interface{})["repo"].(map[string]interface{})["name"].(string)
 	var gitBranchName string = webhookData["pull_request"].(map[string]interface{})["head"].(map[string]interface{})["ref"].(string)
 
+	handleQueue(gitBranchName, gitProjectName)
+
 	f.Println(gitBranchName)
-
-}
-
-func SetupCloseHandler() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		fmt.Println("\r- Ctrl+C pressed in Terminal")
-		os.RemoveAll("./tmp/")
-		f.Println("Exited")
-		os.Exit(0)
-	}()
 }
 
 func main() {
+
+	os.RemoveAll("./tmp/")
 
 	f.Println("Visit https://github.com/levankhelo/pullantis/blob/master/README.md for setup information")
 
@@ -65,60 +192,36 @@ func main() {
 	var tokenPulumi = flag.String("pulumi-token", "", "Pulumi Token")
 	var webhookGit = flag.String("webhook", "/events", "GitHub webhook tag")
 	var localPort = flag.String("port", "4141", "local port for listener")
-	var allowServer = flag.Bool("server", true, "allow server listener")
+	var noServer = flag.Bool("no-server", false, "allow server listener")
 
 	// Parse Arguments
 	flag.Parse()
 
+	// display arg values
 	f.Println("----GIT----\n|\t", "\n|\tUser:", *userGit, "\n|\tToken:", *tokenGit, "\n|\tRepo:", *targetRepo)
 	f.Println("---PULUMI--\n|\t", "\n|\tToken:", *tokenPulumi)
 	f.Println("--WEBHOOK--\n|\t", "Hook:", *webhookGit, "\n|\tPort:", *localPort)
 
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *tokenGit},
-	)
-	tc := oauth2.NewClient(ctx, ts)
+	// init git user using Access Token
+	gitClient, ctx := goGitLogin(*tokenGit)
 
-	gitClient := github.NewClient(tc)
-
-	// list all repositories for the authenticated user
-	repos, _, err := gitClient.Repositories.List(ctx, "", nil)
-	if err != nil {
-		f.Println("Could not authenticate git")
-		return
-	}
-	var repo github.Repository
-	for i := 0; i < len(repos); i++ {
-		f.Println(*repos[i].Name)
-		if *repos[i].Name == *targetRepo {
-			repo = *repos[i]
-		}
-	}
-	// f.Printf("%v\n", repo)
-
+	// get target reository
+	repo := goGetGitRepo(ctx, gitClient, *targetRepo)
+	f.Printf("\n|%v\n\n\n", repo)
 	SetupCloseHandler()
 
 	os.Mkdir("tmp/", 0700)
-
 	os.Chdir("tmp/")
-
-	newRepo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-		URL: *repo.HTMLURL,
-	})
-
-	f.Printf("NEW REPO: %v\n", *newRepo)
-
-	// newRepo.Pull(&git.PullOptions{
-	// 	RemoteName: "origin",
-	// })
-
+	goCloneGitRepo(*tokenGit, &repo)
 	os.Chdir("../")
 
-	if *allowServer {
+	if *noServer != true {
 		log.Println("server started")
 		http.HandleFunc(*webhookGit, handleWebhook)
 		log.Fatal(http.ListenAndServe(":"+(*localPort), nil))
+	} else {
+		log.Println("building without server")
+		os.RemoveAll("./tmp/")
 	}
 
 }
